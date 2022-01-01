9b3e80654cbf51013ab905a16ebf106a81f4d89b
Ahmad Abou Alaynain <ahmadaboualaynian2@gmail.com>
2022-01-01 23:44:18 +0200

    Updates Result Numbers

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,11 +1,11 @@
 const Results = ({ time, wordsCorrect, wordsTotal }) => {
   return (
     <div className="p-5 m-5  flex">
-      <h3>Typing Speed (wpm): {wordsTotal / (time / 60)}</h3>
-      <h3>Accuracy: {(wordsCorrect / wordsTotal) * 100}</h3>
+      <h3>Typing Speed (wpm): {Math.ceil(wordsTotal / (time / 60))}</h3>
+      <h3>Accuracy: {Math.ceil((wordsCorrect / wordsTotal) * 100)}</h3>
       <h3>
         Net Speed (wpm):{" "}
-        {(wordsTotal / (time / 60)) * (wordsCorrect / wordsTotal)}
+        {Math.ceil((wordsTotal / (time / 60)) * (wordsCorrect / wordsTotal))}
       </h3>
     </div>
   );
